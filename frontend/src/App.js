f86380f7048a26ff1307aeb605bae7c932a88193
mbugua <mbuguafaimba@gmail.com>
2023-04-13 13:34:30 +0300

    deleted app.css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import "./App.css"
+
 import Login from "./components/Login"
 import Signup from "./components/Signup"
 import LogOut from "./components/LogOut"
