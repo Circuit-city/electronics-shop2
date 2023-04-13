3e629a10020e35ae1ee62567aded84724c0b2390
Issa Nyale <me@email.com>
2023-04-13 10:01:41 +0300

    Created a Signup page

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,20 +1,20 @@
 import React,{useState} from "react";
 
 function Signup(){
-    const [username, setusername] = useState('')
-    const [email,setemail] = useState('')
-    const [password,setpassword] = useState('')
+    const [name, setname] = useState('');
+    const [email,setemail] = useState('');
+    const [password,setpassword] = useState('');
 
     function handlesubmit(e){
         e.preventDefault()
-        console.log(username,email,password)
+        console.log(name,email,password)
 
-        fetch(`http://localhost:3000`, {
+        fetch(`http://localhost:4000/register`, {
             method: 'POST',
             headers:{
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({username ,email , password})
+            body: JSON.stringify({name ,email , password})
         })
         .then(res => res.json())
         .then(data => {
@@ -28,16 +28,17 @@ function Signup(){
             <center>
                 <h1>Signup</h1>
             </center>
-                <label htmlFor="username" >Username</label>
-                <input type="text" name="username" value={username} placeholder="Enter Username" onChange={(e) => setusername(e.target.value)}/>
+                <label>
+                    Name: <input type="text" name="name" value={name} placeholder="Enter name" onChange={(e) => setname(e.target.value)}/>
+                </label>
+
+                <label>
+                    Email: <input type="text" name="email" value={email} placeholder="Enter email" onChange={(e) => setemail(e.target.value)}/>
+                </label>
                 
-                <label htmlFor="email" >Email</label>
-                <input type="email" name="email" placeholder="Enter Email" value={email} onChange={(e) => setemail(e.target.value)}/>
-                
-                
-                <label htmlFor="password" >Password</label>
-                <input type="password" name="password" placeholder="Enter Password" value={password} onChange={(e) => setpassword(e.target.value)}/>
-                
+                <label>
+                    Password: <input type="password" name="password" placeholder="Enter Password" value={password} onChange={(e) => setpassword(e.target.value)}/>
+                </label>
                 <button type="submit" name="submit" onClick={handlesubmit}>Sign up</button>
             </form>
         </div>
