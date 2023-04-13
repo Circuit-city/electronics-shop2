31ed3001e15c9681b5e0c192f14db357446f73f4
mbugua <mbuguafaimba@gmail.com>
2023-04-13 13:33:18 +0300

    added login and logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import "./App.css"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
+import LogOut from "./components/LogOut"
 
 
 function App() {
@@ -9,8 +10,9 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/logout" element={<LogOut />}/>
+          <Route path="/signup" element={<Signup />}/>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,15 +1,17 @@
 import React,{useState} from "react";
-
+import { useNavigate } from "react-router-dom";
+import '../components/Signup.css'
 function Signup(){
     const [name, setname] = useState('');
     const [email,setemail] = useState('');
     const [password,setpassword] = useState('');
+    const navigate = useNavigate()
 
     function handlesubmit(e){
         e.preventDefault()
         console.log(name,email,password)
 
-        fetch(`http://localhost:4000/register`, {
+        fetch(`http://localhost:3000/register`, {
             method: 'POST',
             headers:{
                 'Content-Type': 'application/json'
@@ -19,17 +21,19 @@ function Signup(){
         .then(res => res.json())
         .then(data => {
             console.log(data);
+            localStorage.setItem('user', JSON.stringify(data.user));
+            navigate('/logout')
         })
     }
 
     return(
-        <div>
-            <form onSubmit={handlesubmit}>
+        <div className="hero">
+            <form onSubmit={handlesubmit} className="sign-form">
             <center>
                 <h1>Signup</h1>
             </center>
                 <label>
-                    Name: <input type="text" name="name" value={name} placeholder="Enter name" onChange={(e) => setname(e.target.value)}/>
+                    Name: <input type="text" name="name" value={name} placeholder="Enter name" onChange={(e) => setname(e.target.value)} />
                 </label>
 
                 <label>
@@ -37,9 +41,9 @@ function Signup(){
                 </label>
                 
                 <label>
-                    Password: <input type="password" name="password" placeholder="Enter Password" value={password} onChange={(e) => setpassword(e.target.value)}/>
+                    Password: <input type="password" name="password" placeholder="Enter Password" value={password} onChange={(e) => setpassword(e.target.value)} />
                 </label>
-                <button type="submit" name="submit" onClick={handlesubmit}>Sign up</button>
+                <button type="submit" name="submit" onClick={handlesubmit} className="sign-btn">Sign up</button>
             </form>
         </div>
     )
