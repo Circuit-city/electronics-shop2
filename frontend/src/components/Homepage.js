fab857db301a4245a2c471a4d5bdcf93df84664f
rose534 <wambuirose534@gmail.com>
2023-04-15 22:55:51 +0300

    homepage css and styling

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -37,14 +37,28 @@ function Homepage() {
       <div id="additional-boxes" className="boxes-container">
         <div className="box box4">
           <i className="fas fa-gift"></i>
+          <img
+            src="https://53525363.000webhostapp.com/Images/Skullcandy_Crusher_Wireless_Over-Ear_Headphones_-_Black-removebg-preview.png"
+            alt=""
+          />
+          <h2>Reasonable Pricing</h2>
           <h3>Special Offers</h3>
         </div>
         <div className="box box5">
-          <i className="fas fa-truck"></i>
-          <h3>Fast Shipping</h3>
+        <img
+            src="https://53525363.000webhostapp.com/Images/HP_Pavilion_15-cs3000_%E8%A3%BD%E5%93%81%E8%A9%B3%E7%B4%B0_-_%E3%83%8E%E3%83%BC%E3%83%88%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3___%E6%97%A5%E6%9C%ACHP-removebg-preview.png"
+            alt=""
+          />
+          <h2>Reasonable Pricing</h2>
+            <h3>Fast Shipping</h3>
         </div>
         <div className="box box6">
           <i className="fas fa-check"></i>
+          <img
+            src="https://53525363.000webhostapp.com/Images/MSI_-_Prestige_14_EVO_14__FHD_Laptop_-_i5-1135G7_-_16GB_Memory_-_IrisXe_-_512GB_SSD_-_Win10Home_-_Rose_Pink-removebg-preview.png"
+            alt=""
+          />
+          <h2>Reasonable Pricing</h2>
           <h3>100% Satisfaction Guarantee</h3>
         </div>
       </div>
