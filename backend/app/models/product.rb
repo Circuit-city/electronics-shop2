252f664fbece6c810e3e9ab3d8f7f1ccca128569
Ian <chemwenokibet11@gmail.com>
2023-04-19 23:49:28 +0300

    created products route

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+
+  def product_image_url
+    image.attached? ? url_for(image) : nil
+  end
+
 end
