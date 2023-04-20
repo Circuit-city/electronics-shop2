71f1aa90de5c6fb35d7e4b0bb87de27c4a843a1d
TimothyKagondu <timothy.2k21@gmail.com>
2023-04-20 14:48:04 +0300

    Completed login component test

--- frontend/src/components/__test__/Login.test.js
+++ frontend/src/components/__test__/Login.test.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Signup from '../Signup';
+import Login from '../Login';
+
+describe('Login', () => {
+    test('renders the Login component', () => {
+      render( Login );
+    });
+  });
+
 test ('test', ()=>{
     expect(true).toBe(true);
 })
+
