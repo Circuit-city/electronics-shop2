aaf6b25f139552df104e06079e0ad9eaab00e76c
rose534 <wambuirose534@gmail.com>
2023-04-20 14:37:40 +0300

    fetching products to homepage

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -78,9 +78,8 @@ function Homepage() {
         <div className="product-cards-container">
       {products.map(product => (
         <div key={product.id} className="product-card">
-          <img src={product.image_url} alt={product.name} />
+          <img src={product.image} alt={product.name} />
           <h3>{product.name}</h3>
-          <p>{product.description}</p>
           <p>Price: {product.price}</p>
         </div>
       ))}
