bf8fb509d171cf54b9136c6c314a9ed5515d3828
Issa Nyale <me@email.com>
2023-04-20 10:13:15 +0300

    Added a logout component

---
+++ frontend/src/components/LogOut.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+const LogOut = ({user}) => {
+  const navigate = useNavigate();
+  
+  const storedUser = localStorage.getItem('user');
+  const loggedInUser = storedUser ? JSON.parse(storedUser) : null;;
+  const handleLogout = async () => {
+    if (!loggedInUser && !user) {
+      // If user is not logged in, redirect to login page
+      navigate("/login");
+      return;
+    }
+
+    const response = await fetch(`https://circuit-city-sxh8.onrender.com/logout`, {
+      method: 'DELETE'
+    });
+
+    if (response.ok) {
+      // Clear user data from local storage 
+      localStorage.removeItem('user');
+      navigate("/login");
+    } else {
+      console.error(response);
+    }
+  }
+  console.log("user in LogOut:", loggedInUser);
+  return (
+    <div>
+      {loggedInUser && (
+        <h1>
+          Welcome <span>{loggedInUser.name}</span>
+        </h1>
+      )}
+      <button onClick={(e) => handleLogout(e)}>logout</button>
+    </div>
+  )
+}
+
+export default LogOut;
+
