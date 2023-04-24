f62337e297bc35c563581e064a8a2a9205d6b266
Ian <chemwenokibet11@gmail.com>
2023-04-24 12:07:12 +0300

    fixed signup route

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,3 +1,82 @@
+# class UsersController < ApplicationController
+#   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+#   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+#   # before_action :set_user, only: %i[ show update destroy ]
+
+#   # GET /users
+#   def index
+#     users = User.all
+
+#     render json: users
+#   end
+
+#   def create
+#     allowed_roles = ['admin', 'user']
+#     role = user_params[:role]
+#     if role.present? && !allowed_roles.include?(role)
+#       render json: { errors: [ 'Invalid role specified']} status: :unprocessable_entity
+#       return
+#     end
+    
+    
+#     begin
+#       user = User.new(user_params)
+#       user.password = params[:password]
+#       user.password_confirmation = params[:password_confirmation]
+#       user.save!
+  
+#       session[:user_id] = user.id
+#       render json: { status: :created, message: "User successfully registered", user: user }
+#     rescue ActiveRecord::RecordInvalid => e
+#       render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+#     rescue ActiveRecord::RecordNotUnique => e
+#       render json: { errors: [e.message] }, status: :unprocessable_entity
+#     end
+#   end
+
+#   # GET /users/1
+#   def show
+#     render json: @user
+#   end
+
+#   # # POST /users
+#   # def create
+#   #   @user = User.new(user_params)
+
+#   #   if @user.save
+#   #     render json: @user, status: :created, location: @user
+#   #   else
+#   #     render json: @user.errors, status: :unprocessable_entity
+#   #   end
+#   # end
+
+#   # PATCH/PUT /users/1
+#   def update
+#     if @user.update(user_params)
+#       render json: @user
+#     else
+#       render json: @user.errors, status: :unprocessable_entity
+#     end
+#   end
+
+#   # DELETE /users/1
+#   def destroy
+#     @user.destroy
+#   end
+
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_user
+#       @user = User.find(params[:id])
+#     end
+
+#     # Only allow a list of trusted parameters through.
+#     def user_params
+#       params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+#     end
+# end
+
+
 class UsersController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
@@ -11,12 +90,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    allowed_roles = ['admin', 'user']
+    role = user_params[:role]
+    if role.present? && !allowed_roles.include?(role)
+      render json: { errors: ['Invalid role specified'] }, status: :unprocessable_entity
+      return
+    end
+
     begin
       user = User.new(user_params)
+      user.role = role if role.present?
       user.password = params[:password]
       user.password_confirmation = params[:password_confirmation]
       user.save!
-  
+
       session[:user_id] = user.id
       render json: { status: :created, message: "User successfully registered", user: user }
     rescue ActiveRecord::RecordInvalid => e
@@ -30,17 +117,6 @@ class UsersController < ApplicationController
   def show
     render json: @user
   end
-
-  # # POST /users
-  # def create
-  #   @user = User.new(user_params)
-
-  #   if @user.save
-  #     render json: @user, status: :created, location: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
 
   # PATCH/PUT /users/1
   def update
@@ -64,6 +140,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
     end
 end
+
