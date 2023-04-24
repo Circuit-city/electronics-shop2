ac17836d48c5e81ea8f71bcf386553868fb280dc
brian <brianmakorious@gmail.com>
2023-04-24 14:59:23 +0300

    done with checkout button

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -5,15 +5,21 @@ function Cart() {
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    const cartItemsFromLocalStorage = localStorage.getItem('cartItems');
-    if (cartItemsFromLocalStorage) {
-      setCartItems(JSON.parse(cartItemsFromLocalStorage));
-    }
+    // const cartItemsFromLocalStorage = localStorage.getItem('cartItems');
+    // if (cartItemsFromLocalStorage) {
+    //   setCartItems(JSON.parse(cartItemsFromLocalStorage));
+    // }
+    fetch('https://circuit-cityy-po9y.onrender.com/products')
+    .then(response => response.json())
+    .then(data => setCartItems(data))
+    .catch(error => console.error('Error fetching products:', error));
+    
   }, []);
+  console.log(cartItems)
 
-  useEffect(() => {
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-  }, [cartItems]);
+  // useEffect(() => {
+  //   localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  // }, [cartItems]);
 
   const addToCart = (product) => {
     const existingItem = cartItems.find((item) => item.id === product.id);
@@ -69,13 +75,16 @@ function Cart() {
       <Navbar />
       {cartItems.map((item) => (
         <div key={item.id}>
-          <img src={item.image} alt={item.name} />
+         <img src={item.image} alt={item.name} style={{ width: '300px', height: '200px' }} />
+
           <h2>{item.name}</h2>
           <p>{item.description}</p>
           <p>Price: {item.price}</p>
           <p>Quantity: {item.quantity}</p>
-          <button onClick={() => increaseQuantity(item.id)}>+</button>
-          <button onClick={() => decreaseQuantity(item.id)}>-</button>
+          <button className="btn btn-primary"  onClick={() => increaseQuantity(item.id)}>+</button>
+          <button className="btn btn-danger"  onClick={() => decreaseQuantity(item.id)}>-</button>
+          <button className="btn btn-success"  onClick={() => alert('add check out here ESIR!!!!!!!!')}>Checkout</button>
+
         </div>
       ))}
     </div>
