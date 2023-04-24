abf7344d29b765635fcce69bc213e6834424fdea
brian <brianmakorious@gmail.com>
2023-04-24 10:29:55 +0300

    updating products

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -7,24 +7,75 @@ function Cart() {
   useEffect(() => {
     const cartItemsFromLocalStorage = localStorage.getItem('cartItems');
     if (cartItemsFromLocalStorage) {
-      setCartItems(Object.values(JSON.parse(cartItemsFromLocalStorage)));
+      setCartItems(JSON.parse(cartItemsFromLocalStorage));
     }
   }, []);
 
-  return (
-    <div>
-      <Navbar/>
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
 
-      {cartItems.map(item => (
+  const addToCart = (product) => {
+    const existingItem = cartItems.find((item) => item.id === product.id);
+    if (existingItem) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === existingItem.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const increaseQuantity = (itemId) => {
+    setCartItems(
+      cartItems.map((item) =>
+        item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decreaseQuantity = (itemId) => {
+    const existingItem = cartItems.find((item) => item.id === itemId);
+    if (existingItem.quantity > 1) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === itemId ? { ...item, quantity: item.quantity - 1 } : item
+        )
+      );
+    } else {
+      setCartItems(cartItems.filter((item) => item.id !== itemId));
+    }
+  };
+
+  const getTotalPrice = () => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+
+  const checkout = () => {
+    // implement checkout logic here
+    setCartItems([]);
+    alert('Thank you for your order!');
+  };
+
+  return 
+    <div>
+      <Navbar />
+      {cartItems.map((item) => (
         <div key={item.id}>
           <img src={item.image} alt={item.name} />
           <h2>{item.name}</h2>
           <p>{item.description}</p>
           <p>Price: {item.price}</p>
+          <p>Quantity: {item.quantity}</p>
+          <button onClick={() => increaseQuantity(item.id)}>+</button>
+          <button onClick={() => decreaseQuantity(item.id)}>-</button>
         </div>
       ))}
-    </div>
-  );
-}
-
-export default Cart;
+      <
