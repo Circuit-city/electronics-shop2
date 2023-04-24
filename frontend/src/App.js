56830d6860c705c86f7d999566d9dffacf9e9101
Issa Nyale <me@email.com>
2023-04-24 11:22:56 +0300

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./components/Login"
 import Signup from "./components/Signup"
 import LogOut from "./components/LogOut"
-import Chekout from "./components/Checkout"
-
+import Cart from './components/Cart';
+import Homepage from "./components/Homepage"
+import Checkout from "./components/Checkout"
 
 function App() {
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
+        <Route path="/" element={<Homepage />} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/login" element={<Login />}/>
           <Route path="/logout" element={<LogOut />}/>
-          <Route path="/checkout" element={<Chekout />}/>
+          <Route path="/signup" element={<Signup />}/>
+          <Route path="/checkout" element={<Checkout />}/>
         </Routes>
       </BrowserRouter>
     </div>
   )
 }
 
- export default App
+export default App;
