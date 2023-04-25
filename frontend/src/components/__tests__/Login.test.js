b5f0a54afc3aee98897e2751be01274dc3f11b88
rose534 <wambuirose534@gmail.com>
2023-04-25 13:28:05 +0300

    started tests

---
+++ frontend/src/components/__tests__/Login.test.js
@@ -0,0 +1,12 @@
+import Login from '../Login';
+
+describe('Login', () => {
+    test('renders the Login component', () => {
+      ( Login );
+    });
+  });
+
+
+  test ('test', ()=>{
+    expect(true).toBe(true);
+})
---
+++ frontend/src/components/__tests__/Signup.test.js
@@ -0,0 +1,11 @@
+import Signup from '../Signup';
+
+describe('signup', () => {
+    test('renders the Signup component', () => {
+     ( Signup );
+    });
+  });
+
+test ('test', ()=>{
+    expect(true).toBe(true);
+})
