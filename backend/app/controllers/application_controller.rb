007ebf3e9e0d4e0980704acb35f4d14f6d28cf57
Ian <chemwenokibet11@gmail.com>
2023-05-01 11:34:01 +0300

    token

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -7,13 +7,23 @@ class ApplicationController < ActionController::API
         @products = Product.all
     end    
 
+    # def require_admin
+    #     if current_user && current_user.admin?
+    #       payload = {user_id: current_user.id, admin: true}
+    #       jwt_token = JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    #       render json: {jwt: jwt_token}
+    #     else
+    #       render json: {error: 'You need Admin privileges'}, status:  :unauthorized       
+    #      end         
+    # end
+
     def require_admin
-        if current_user && current_user.admin?
-          payload = {user_id: current_user.id, admin: true}
-          jwt_token = JWT.encode(payload, Rails.application.secrets.secret_key_base)
-          render json: {jwt: jwt_token}
-        else
-          render json: {error: 'You need Admin privileges'}, status:  :unauthorized       
-        end    
+      token = request.headers["Authorization"]&.split&.last
+      decoded_token = JWT.decode(token, 'my_secret_key', true, { algorithm: 'HS256' })
+      is_admin = decoded_token.first['is_admin']
+      unless is_admin
+        render json: { error: "You need Admin privileges" }, status: :unauthorized
+      end
     end
+    
 end
